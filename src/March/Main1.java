829f432f239605431458fb44a67e73273e055e7d
Altun <a.ozder@outlook.com>
2021-02-27 13:09:57 -0500

    March

---
+++ src/March/Main1.java
@@ -0,0 +1,8 @@
+package March;
+
+public class Main1 {
+    public static void main(String[] args) {
+        String[]a={"A","B","C"};
+        System.out.println (a[3]);
+    }
+}
---
+++ src/March/Main2.java
@@ -0,0 +1,17 @@
+package March;
+
+public class Main2 {
+    public static void main(String[] args) {
+        doSomething ();
+    }
+
+    public static void doSomething() {
+        System.out.println (doAnotherThing ());
+
+    }
+
+    public static String doAnotherThing() {
+        return"ABC";
+
+    }
+}
